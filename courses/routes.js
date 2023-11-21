35df7e9fc0152ef8c592c3dde83c30854865f712
Z-Langelaan <zoe.langelaan@casema.nl>
2023-11-20 23:47:32 -0500

    kanbas

--- courses/routes.js
+++ courses/routes.js
@@ -1,5 +1,17 @@
 import Database from "../Database/index.js";
 function CourseRoutes(app) {
+
+  app.get("/api/courses/:id", (req, res) => {
+    const { id } = req.params;
+    const course = Database.courses
+      .find((c) => c._id === id);
+    if (!course) {
+      res.status(404).send("Course not found");
+      return;
+    }
+    res.send(course);
+  });
+
   app.post("/api/courses", (req, res) => {
     const course = { ...req.body, _id: new Date().getTime().toString() };
     Database.courses.push(course);
