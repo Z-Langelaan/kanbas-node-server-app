1436e35390731915dcc7ad6adec78d2a822c0ff6
Z-Langelaan <zoe.langelaan@casema.nl>
2023-12-02 18:05:23 -0500

    full user implementation

--- app.js
+++ app.js
@@ -2,11 +2,28 @@ import express from 'express'
 import Hello from "./hello.js"
 import Lab5 from "./lab5.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import session from "express-session";
 const app = express()
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+}));
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  app.use(
+    session(sessionOptions)
+  );
+  
 app.use(express.json());
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 Lab5(app);
